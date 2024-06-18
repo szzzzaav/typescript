d61d9dda2e3930112c08be1f2e7a713f54b0c46f
szzzzaav <sulaimuzzz@sina.com>
2024-06-18 20:25:58 +0800

    type

---
+++ index.js
@@ -0,0 +1,7 @@
+let str = "abc";
+let num = 123;
+let v = null;
+let u = undefined;
+console.log(str, num);
+u = v;
+console.log(u);
---
+++ index.ts
@@ -0,0 +1,8 @@
+let str: string = "abc";
+let num: number = 123;
+let v: void = null;
+let u: void = undefined;
+console.log(str, num);
+
+u = v;
+console.log(u);
