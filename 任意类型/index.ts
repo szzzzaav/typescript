594a61508736b11b4b688fcc103350a4f1d66e49
szzzzaav <sulaimuzzz@sina.com>
2024-06-19 14:18:52 +0800

    type

--- 任意类型/index.ts
+++ 任意类型/index.ts
@@ -12,5 +12,28 @@
 // let unk: unknown = { xxx: 123 };
 //ERR console.log(unk.xxx);
 
-let any: any = { xxx: 123 };
-console.log(any.xxx);
+// let any: any = { xxx: 123 };
+// console.log(any.xxx);
+
+// 原型链：所有类型的原型链顶层都是Object,所有Object可以是任何类型
+// let a1: Object = 123;
+// let a2: Object = [];
+// let a3: Object = {};
+// let a4: Object = () => 123;
+// let a5: Object = "123";
+
+// // console.log("compile pass");
+// // 泛型约束：非原始类型的类型，只支持引用类型
+// let b1: object = 123;
+// // 错误 数字是原始类型
+// let b2: object = "123";
+// // 错误 字符串是原始类型
+// console.log("compile pass");
+
+// // 引用类型
+// let b3: object = [];
+// let b4: object = {};
+// let b5: object = () => 123;
+
+let c: {} = { name: "xxx" };
+// c.age = 18;错误
