4228b40c54f76d31d7ab9087095543b112e596dc
szzzzaav <sulaimuzzz@sina.com>
2024-06-20 14:49:44 +0800

    rain

---
+++ 内置对象/codeRain.ts
@@ -0,0 +1,25 @@
+let canvas: HTMLCanvasElement = document.querySelector("canvas");
+
+let ctx = canvas.getContext("2d");
+canvas.width = screen.availWidth;
+canvas.height = screen.availHeight;
+
+let str: string[] = "SULAIMUZZZ0101".split("");
+let Arr: Array<number> = Array(Math.ceil(canvas.width / 10)).fill(0);
+
+const rain = () => {
+  ctx.fillStyle = "rgba(0,0,0,0.05)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#0f0";
+  Arr.forEach((item, index) => {
+    ctx.fillText(
+      str[Math.floor(Math.random() * str.length)],
+      index * 10,
+      item + 10
+    );
+    Arr[index] =
+      item > canvas.height || item > 4000 * Math.random() ? 0 : item + 10;
+  });
+};
+
+setInterval(rain, 40);
--- 内置对象/index.ts
+++ 内置对象/index.ts
@@ -1,28 +1,28 @@
 // ECMAScript
 
-import { Session } from "inspector";
+// import { Session } from "inspector";
 
-let num: Number = new Number(1);
-let date: Date = new Date();
-let reg: RegExp = new RegExp();
-let xhr: XMLHttpRequest = new XMLHttpRequest();
+// let num: Number = new Number(1);
+// let date: Date = new Date();
+// let reg: RegExp = new RegExp();
+// let xhr: XMLHttpRequest = new XMLHttpRequest();
 
-// Dom
-// 元素名称
-let div: HTMLDivElement = document.querySelector("div");
-let input: HTMLInputElement = document.querySelector("input");
+// // Dom
+// // 元素名称
+// let div: HTMLDivElement = document.querySelector("div");
+// let input: HTMLInputElement = document.querySelector("input");
 
-let section: HTMLElement = document.querySelector("section");
+// let section: HTMLElement = document.querySelector("section");
 
-let footers: NodeList = document.querySelectorAll("footers");
+// let footers: NodeList = document.querySelectorAll("footers");
 
-let div: NodeListOf<HTMLElement | HTMLDivElement> =
-  document.querySelector("div");
+// let div: NodeListOf<HTMLElement | HTMLDivElement> =
+//   document.querySelector("div");
 
-// BOM
-let local: Storage = localStorage;
-let session: Storage = sessionStorage;
-let lo: Location = location;
+// // BOM
+// let local: Storage = localStorage;
+// let session: Storage = sessionStorage;
+// let lo: Location = location;
 
-// 接受返回的类型
-let promise: Promise<number> = new Promise((r) => r(1));
+// // 接受返回的类型
+// let promise: Promise<number> = new Promise((r) => r(1));
